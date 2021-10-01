273e8fc1cba5b6d66877e4bbde978fd84fecb387
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-01 14:28:47 +1000

    added axios dictionary api integration

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 import "./Search.css";
+import axios from "axios";
+import Dictionary from "./Dictionary";
 
 export default function Search() {
   const [keyword, setKeyword] = useState("");
+  const [dictionaryData, setDictionaryData] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setDictionaryData({
+      word: response.data[0].word,
+      phonetic: response.data[0].phonetic,
+      origin: response.data[0].origin,
+      meaning: {
+        definitionOne: {
+          definition: response.data[0].meanings[0].definitions[0].definition,
+          example: response.data[0].meanings[0].definitions[0].example,
+        },
+        definitionTwo: {
+          definition: response.data[0].meanings[0].definitions[1].definition,
+          example: response.data[0].meanings[0].definitions[1].example,
+        },
+      },
+    });
+    console.log(dictionaryData);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
   function search(event) {
     event.preventDefault();
-    console.log(`searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function updateWord(response) {
     setKeyword(response.target.value);
@@ -24,6 +49,7 @@ export default function Search() {
           ></input>
           <input className="btn btn-light" type="submit" value="search"></input>
         </form>
+        <Dictionary data={dictionaryData} />
       </div>
     </div>
   );
