12912c1d31d86ee2c7a0ee0d6dea1f2e005784dc
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-05 10:49:08 +1100

    updated return callback frunction of photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -6,24 +6,22 @@ export default function Photos(props) {
     return (
       <div className="Photos row">
         {props.photos.map(function (photo, index) {
-          if (index < 9) {
-            return (
-              <div className="col-sm-4 mb-4">
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    className=" img-fluid"
-                    key={index}
-                    src={photo.src.landscape}
-                    alt={props.keyword}
-                  />
-                </a>
-              </div>
-            );
-          }
+          return (
+            <div className="col-sm-4 mb-4">
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <img
+                  className=" img-fluid"
+                  key={index}
+                  src={photo.src.landscape}
+                  alt={props.keyword}
+                />
+              </a>
+            </div>
+          );
         })}
       </div>
     );
   } else {
-    return "";
+    return null;
   }
 }
