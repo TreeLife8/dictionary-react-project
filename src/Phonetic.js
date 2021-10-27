0e65eee41b1d1e4e395670bb997b1bf8950ed2e0
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-27 13:13:16 +1100

    updated audio into a button to play without redirection

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,18 +3,20 @@ import "./Phonetic.css";
 
 export default function Phonetic(props) {
   if (props.phonetic) {
+    function playAudio() {
+      new Audio(`${props.phonetic.audio}`).play();
+    }
     return (
       <div className="Phonetic">
         <h4 className="phonetic">
           <em>"{props.phonetic.text}"</em>
         </h4>{" "}
         {props.phonetic.audio !== undefined ? (
-          <h4 className="phonetic-audio">
-            <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-              {" "}
+          <button className="audio">
+            <h4 onClick={playAudio} className="phonetic-audio">
               listen 🔉
-            </a>
-          </h4>
+            </h4>
+          </button>
         ) : (
           ""
         )}
